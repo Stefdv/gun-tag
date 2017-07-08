e02324d4a43feb9c775dcaf727e2c5ee7a2411de
Stefdv <stefdv2@hotmail.com>
2017-07-08 12:37:22 +0200

    updated

--- gun-tag.js
+++ gun-tag.js
@@ -1,5 +1,6 @@
 /**
  * guntagger
+ * @version 2.0.2 ( optimized for Gun ^0.8.x )
  * @author  S.J.J. de Vries ( Stefdv2@hotmail.com)
  * @gitter @Stefdv
  * @purpose Add tagging capabilities to Gun
