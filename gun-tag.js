4b7709bc069fbd626c05f4ed25c69b90658f6f12
Stefdv <stefdv2@hotmail.com>
2019-11-10 21:03:42 +0100

    Update gun-tag.js

--- gun-tag.js
+++ gun-tag.js
@@ -3,31 +3,37 @@
  * @author  S.J.J. de Vries ( Stefdv2@hotmail.com)
  * @gitter @Stefdv
  * @purpose Add tagging capabilities to Gun
- * 
- * @version 2.0.5
+ *
+ * @version 3.0.0
  * Now up to date for Gun v0.9.x.
- * 
+ *
  * @dependencies
  * Gun
  * gun-synclist
  */
+
 ;(function(){
+  console.log("Also thanks for using - or at least trying - gun-tag.")
   if(typeof window !== "undefined"){
     var Gun = window.Gun;
   } else {
     var Gun = require('gun/gun');
   }
- try{ Gun.chain.synclist || require('gun-synclist/gun-synclist'); }
- catch(e){console.log(e)}
 
-  const _scopes = '__scopes';
-  let _scope = 'gun-tag/';
+  const _scope = "gun-tag/";
+  const _scopes = "_scopes";
 
   const invalid = value => {
-     if (!Gun.obj.is(value) || !value._) {
-      console.warn('Only nodes can be tagged');
-      return true;
-    };
+    if (!Gun.obj.is(value) || !value._) {
+     console.warn('Only nodes can be tagged');
+     return true;
+   };
+ };
+
+  const validateNode = (node,tag) => {
+    return Object.keys(node).reduce(function(previous, current) {
+          return (node.tags && node.tags[tag] === 1);
+      }, {});
   };
 
   const serialize = (gun,tags,method) => {
@@ -35,47 +41,44 @@
     tags.forEach( tag => gun[method](tag) )
   };
 
-  const isScope = (gun,tag) => {
-    return new Promise(resolve => {
-      getScopes(gun).then( sc => { 
-        resolve(Object.keys(sc).includes(tag))
-      })
-    })
-  };
- 
-  const validateNode = (node,tag) => {
-    return Object.keys(node).reduce(function(previous, current) {
-          return (node.tags && node.tags[tag] === 1);
-      }, {});
-  };
-
-  const getScope = () => {
-    return _scope.split('/')[0]
-  };
-
   const getScopes = (gun) => {
     return new Promise(resolve => {
       let root = gun.back(-1);
-      root.get( getScope() + '/__scopes' ).once( sc => { 
+      root.get(_scope + _scopes ).once( sc => {
         delete ((sc = Gun.obj.copy(sc))||{})._;
         resolve(sc)
       })
     })
+
+  };
+
+  const isScope = (gun,tag) => {
+    return new Promise(resolve => {
+      getScopes(gun)
+      .then( sc => {
+        resolve(Object.keys(sc).includes(tag))
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    })
   };
 
   Gun.chain.tag = function (tag) {
-    if(!tag) { return this};
+    if(!tag || typeof(tag) === "number") { return this};
+    if (Gun.list.is(tag) ) { return serialize(this, tag, 'tag');}
     let gun = this.back(-1);
     let nodeSoul,scopeSoul,newScope,newTag;
-    if (Gun.list.is(tag) ) { return serialize(this, tag, 'tag');}
 
-    return this.once(function (node) { 
+    return this.once(function (node) {
       if (invalid(node)) { return this;}
       nodeSoul= Gun.node.soul(node);
 
+      // consider a tag with slash ("Books/Fantasy") a scoped
+      // tag
       if(tag.includes('/')) {
-        newScope = tag.split('/')[0]; 
-        newTag   = tag.split('/')[1]; 
+        newScope = tag.split('/')[0];
+        newTag   = tag.split('/')[1];
         gun.get(_scope+'TAGS').get(newScope).put({'#': newScope+'/TAGS'});
         gun.get(_scope + _scopes).get(newScope).put(1);
         gun.get(newScope+'/TAGS').get(newTag).put({'#':tag});
@@ -85,7 +88,37 @@
         gun.get(_scope+tag).get(nodeSoul).put({'#':nodeSoul});
       };
       this.get('tags').get(tag).put(1);
-    });
+    })
+  };
+  /* Gun.chain.path is in the lib folder of Gun,
+     but i'm not sure if it's there to stay ;) */
+  Gun.chain.path = function(field, opt){
+    var back = this, gun = back, tmp;
+    if(typeof field === 'string'){
+      tmp = field.split(opt || '.');
+      if(1 === tmp.length){
+        gun = back.get(field);
+        return gun;
+      }
+      field = tmp;
+    }
+    if(field instanceof Array){
+      if(field.length > 1){
+        gun = back;
+        var i = 0, l = field.length;
+        for(i; i < l; i++){
+          gun = gun.get(field[i]);
+        }
+      } else {
+        gun = back.get(field[0]);
+      }
+      return gun;
+    }
+    if(!field && 0 != field){
+      return back;
+    }
+    gun = back.get(''+field);
+    return gun;
   };
 
   Gun.chain.untag = function (tag) {
@@ -94,9 +127,15 @@
       return serialize(this, arguments,'untag');
     };
     return this.once(function (node) {
-      if (invalid(node)) {  return this; };
+      if (invalid(node)) { return this;}
       node[tag] ? this.get(tag).put(false) : this.get('tags').get(tag).put(0);
     });
+  };
+
+  Gun.chain.switchtag = function(from,to) {
+    if(!Gun.text.is(from)||!Gun.text.is(to)) { return this; };
+    this.untag(from);
+    this.tag(to)
   };
 
   Gun.chain.proptag = function(tag){
@@ -117,89 +156,77 @@
   Gun.chain.tagged = function(tag,cb,opt) {
     let gun = this;
     cb = cb || function(){};
-    opt=opt || {}
+    opt=opt || {};
 
-    if(arguments.length === 0) { return gun.get(_scope + 'TAGS'); };
-
-    if(Gun.text.is(tag) && arguments.length === 1) {
-      if(tag.includes('/') ) { return gun.get(tag); }
-      if(isScope(gun,tag) ) { return gun.get(tag + '/TAGS'); }
-      return gun.get(_scope + tag); 
+    if(arguments.length === 0) {
+      return gun.get(_scope + 'TAGS'); // return all tagnames
     };
 
-    if(Gun.list.is(tag) && arguments.length === 1) {
-      console.warn(
-        `Intersect can only be applied with a callbackb.\nPlease try...\n"gun.tagged(${Gun.text.ify(tag)}, cb)"
-        `);
-      return gun;
-    };
-
-    if(Gun.list.is(tag) && arguments.length > 1) {
-      let matchTags = tag;
-      gun.tagged(matchTags[0],list=>{
-        list = list.reduce(function(result, node) {
-          let count = 0;
-          matchTags.forEach(_tag => {
-            if(node.tags[_tag] && node.tags[_tag] === 1) {
-              count++;
-              if(count == matchTags.length) { result.push(node) }
-            };
-          }); 
-          return result;
-        }, []);
-        cb.call(gun,list)
-      });
-    };
-
-    if(!Gun.text.is(tag) && !Gun.list.is(tag)) { 
-      console.warn( 'tag needs to be a String or an Array!'); 
-      return gun;
-    };
-
-    if(tag.includes('/TAGS')) {
-      gun.get(tag).once( data => {
+    if(arguments.length === 1 && typeof(arguments[0]=='function')){
+        gun.get(_scope+'TAGS').once(data=>{
+          delete ((data = Gun.obj.copy(data))||{})._;
+          arguments[0].call(gun,data)
+      })
+    } else if(Gun.list.is(tag)) {
+      console.info(`You provided an Array, you might want to use 'gun.intersect(${tag})'`)
+      return this
+     } else if(tag.includes('/TAGS')) {
+      gun.get(tag).listonce( data => {
         cb.call(gun,data)
       })
-    } 
-    else if(tag.includes('/') ) {
+    } else if(tag.includes('/')) {
       gun.get(tag).listonce(data => {
-        // data.lookup contains untagged nodes ( tag==0 ) also
-        // so it will be out of sync with the data.list !!
-        // should i built my lookup in here ?
+        // data.lookup and data.list contains untagged nodes ( tag==0 ) also
+        // we need to filter the nodes and only return tag==1 nodes
         data.lookup={};
-        data.list = data.list.reduce(function(list, node) {
-          if(validateNode(node,tag)){ 
+        data.list = data.list.reduce((list, node)=>{
+          if(validateNode(node,tag)){
             data.lookup[node._soul] = list.length;
             list.push(node);
-          } 
+          }
           return list;
         }, []);
- 
         cb.call(gun,data);
       })
-    }
-    else { 
-      isScope(gun,tag).then( _isScope => {
-        if(_isScope) { 
-          gun.get(tag + '/TAGS').once(data => {
-            delete ((data = Gun.obj.copy(data))||{})._;
+    } else {
+      isScope(gun,tag)
+        .then( _isScope => {
+          if(_isScope) {
+            gun.get(tag + '/TAGS').once(data => {
+              delete ((data = Gun.obj.copy(data))||{})._;
+                cb.call(gun,data)
+            })
+          } else {
+            gun.get(_scope + tag ).listonce( data => {
               cb.call(gun,data)
-          })
-        } else {
-          gun.get(_scope + tag ).listonce( data => {
-            
-            data.lookup={};
-            data.list = data.list.reduce(function(list, node) {
-              if(validateNode(node,tag)){ 
-                data.lookup[node._soul] = list.length;
-                list.push(node);
-              } 
-              return list;
-            }, []);
-            cb.call(gun,data)
-          });
-        };
-      });
+            },{filtertags:true});
+          };
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   };
+
+  Gun.chain.intersect = function(tags,cb) {
+    if(!Gun.list.is(tags) || arguments.length==1) {
+      console.warn("for intersects you need to provide an Array with - existing - tags AND a callback")
+      return this
+    }
+    let matchTags = tags;
+    gun.tagged(matchTags[0],data=>{
+      data.list = data.list.reduce(function(result, node) {
+        let cnt = 0;
+        matchTags.forEach(tag => {
+          if(node.tags[tag] && node.tags[tag]===1) {
+            cnt++;
+            if(cnt == matchTags.length) { result.push(node)}
+          };
+        });
+        return result;
+      },[]);
+      cb.call(gun,data.list)
+    });
+  };
+
 }());
